a5a33f3822157ad1581cdf94d5b14c1b72906a54
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2020-11-14 16:52:57 +0100

    initial commit

---
+++ app/js/intersectionObserver.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    function setBorder(getId) {
+        $('.link' + getId).css('font-weight', '500');        
+    }
+
+    function clearBorder(getId) {
+        $('.link' + getId).css('font-weight', '300');        
+    }
+
+    const header = document.querySelector('header');    
+    const modell = document.querySelector('.modell');
+    const angebote = document.querySelector('.angebote');
+    const about = document.querySelector('.about');    
+    const footer = document.querySelector('footer');
+
+    const options = {
+        rootMargin: '-25px',
+        threshold: [0.2, 0.5, 1]
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                setBorder(entry.target.classList[0].charAt(4));
+            } else {
+                clearBorder(entry.target.classList[0].charAt(4));
+            }
+        })
+    }, options);
+
+    
+    observer.observe(header, {threshold: [0.5]});
+    observer.observe(modell, {threshold: [0.5]});
+    observer.observe(angebote, {threshold: [0.2]});
+    observer.observe(about, {threshold: [1]});
+    observer.observe(footer, {threshold: [1]});
+});
---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var navOpen = false;
+    var transY = 2.5;
+    $('.nav__btn-container').click(function () {
+        if (!navOpen) {
+            // nav__btn animation start                     
+            $('.nav__btn-container').addClass('open');
+
+            //dropdown menu animation start
+            $('.nav__link').css('opacity', '1');
+            for (var i = 2; i <= 5; i++) {
+                $('.link' + i).css('transform', 'translateY(' + (transY * (i - 1)) + 'em)');
+            };
+            navOpen = true;
+        } else {
+            //dropdown menu animation start
+            for (var i = 2; i <= 5; i++) {
+                $('.link' + i).css('transform', 'translateY(0)');
+            };
+            $('.nav__link').css('opacity', '0');
+            navOpen = false;
+            
+            // nav__btn animation start   
+            $('.nav__btn-container').removeClass('open');
+
+        }
+
+    });
+
+
+
+});
