4116be44d3fa49b57b23ea3733544018b31ecc7f
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-01-09 16:20:04 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,92 @@
 $(document).ready(function () {
+/*
+    //Interssecction Observer
 
+    const rotateLink = (id) => {
+        $('.nav__link--' + id).removeClass('link-up');
+        $('.nav__link--' + id).siblings().addClass('bottom-down');
+    }
+
+    const backToDefault = (id) => {
+        $('.nav__link--' + id).addClass('link-up');
+        $('.nav__link--' + id).siblings().removeClass('bottom-down');
+    }
+
+    const sections = document.querySelectorAll('section');
+
+    const options = {
+        threshold: 0.4
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                console.log(entry.target);
+
+                rotateLink(entry.target.classList[0]);
+
+                //start slider if #hero is visible
+                //jenky slider animation (überspringt bild)
+                if(entry.target.classList[0] === "hero") {
+                    console.log("hero in");
+                    var interval = setInterval(() => {
+                        runSlider();
+                        if (turnCounter < 5) {
+                            turnCounter++;
+                        } else {
+                            turnCounter = 1;
+                        }
+                    }, 4000);
+
+                }
+                
+            } else {
+                backToDefault(entry.target.classList[0]);
+                if(entry.target.classList[0] === "hero") {
+                    console.log("hero out");
+                    clearInterval(interval);
+                }
+            }
+        })
+    }, options);
+
+    
+
+    sections.forEach(section => {
+        observer.observe(section);
+    })
+*/
+    //nav visibility animation on mobile
+    var navOpen = false;
+
+    const burgerOpen = () => {
+        $('.burger-menu__line--middle').css('opacity', '0');
+        $('.burger-menu__line--top').css('transform', 'translateY(10px) rotate(45deg)');
+        $('.burger-menu__line--bottom').css('transform', 'translateY(-10px) rotate(-45deg');        
+    }
+
+    const burgerClose = () => {
+        $('.burger-menu__line--middle').css('opacity', '1');
+        $('.burger-menu__line--top').css('transform', 'rotate(0)');
+        $('.burger-menu__line--bottom').css('transform', 'rotate(0');        
+    }
+
+    $('.burger-menu').click(() => {
+        if (!navOpen) {
+            burgerOpen();
+            
+            $('.nav-container').css('width', '100%');
+            $('.nav-container').css('height', '100%');            
+            navOpen = true;
+        }else{
+            $('.nav-container').css('width', '3.125rem');
+            $('.nav-container').css('height', '3.125rem');
+                        
+            burgerClose();
+            navOpen = false;
+        }
+    });
 
     //animation of nav__links
     var turn = false;
@@ -15,12 +102,17 @@ $(document).ready(function () {
         }
     });
 
+    
+
+
+
     //slideranimation
     var turnCounter = 1;
     var removeCounter = 1;
     var addCounter = 2;
 
     const runSlider = () => {
+        console.log("slider is running");
         removeCounter = turnCounter;
         if (turnCounter < 5) {
             addCounter = turnCounter + 1;
@@ -49,18 +141,10 @@ $(document).ready(function () {
         runSlider();
         if (turnCounter < 5) {
             turnCounter++;
-          } else {
+        } else {
             turnCounter = 1;
-          }
+        }
     }, 4000);
-
-
-
-
-
-
-
-
 
     // show selected gallery-image in modal
     const modal = $('.modal'),
@@ -100,9 +184,4 @@ $(document).ready(function () {
         closeModal();
         imgWrapper.empty();
     });
-
-
-
-
-
 });
