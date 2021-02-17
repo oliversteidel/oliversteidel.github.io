c5a8ca43fc08a769fa859f8ff40a031a049674cb
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-02-17 10:59:50 +0100

    test view

---
+++ js/intersectionObserver.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    const rotateLink = (id) => {
+        $('.nav__link--' + id).removeClass('link-up');
+        $('.nav__link--' + id).siblings().addClass('bottom-down');
+    }
+
+    const backToDefault = (id) => {
+        $('.nav__link--' + id).addClass('link-up');
+        $('.nav__link--' + id).siblings().removeClass('bottom-down');
+    }
+
+    
+
+    const threshold = 0.5;
+
+    document.querySelectorAll('section').forEach(el => {
+        const elHeight = el.getBoundingClientRect().height;
+        var th = threshold;
+
+        // if element is too tall to ever hit the threshold - change threshold
+
+        if (elHeight > (window.innerHeight * threshold)) {
+            th = ((window.innerHeight * threshold) / elHeight) * threshold;
+        }
+
+        new IntersectionObserver(iEls => iEls.forEach(iEl => {
+            if (iEl.isIntersecting) {
+                rotateLink(iEl.target.classList[0]);
+            }else {
+                backToDefault(iEl.target.classList[0]);
+            }
+        }), {threshold: th}).observe(el);
+    })
+
+    
+});
--- js/main.js
+++ js/main.js
@@ -1,73 +1,164 @@
 $(document).ready(function () {
+
+    //nav visibility animation on mobile
     var navOpen = false;
-    var transY = 2.5;
 
-    //open nav-dropdown-menu on mobile devices
-    $('.nav__btn-container').click(function () {
+    //transform Burger Icon to cross
+    const burgerOpen = () => {
+        $('.burger-menu__line--middle').css('opacity', '0');
+        $('.burger-menu__line--top').css('transform', 'translateY(10px) rotate(45deg)');
+        $('.burger-menu__line--bottom').css('transform', 'translateY(-10px) rotate(-45deg');
+    }
+
+    //tranform Burger Icon to default
+    const burgerClose = () => {
+        $('.burger-menu__line--middle').css('opacity', '1');
+        $('.burger-menu__line--top').css('transform', 'rotate(0)');
+        $('.burger-menu__line--bottom').css('transform', 'rotate(0');
+    }
+
+    $('.burger-menu').click(() => {
         if (!navOpen) {
-            // nav__btn animation start                     
-            $('.nav__btn-container').addClass('open');
-
-            //dropdown menu animation start
-            $('.nav__link').css('opacity', '1');
-            for (var i = 2; i <= 5; i++) {
-                $('.link' + i).css('transform', 'translateY(' + (transY * (i - 1)) + 'em)');
-            };
+            burgerOpen();
+            gsap.to(".nav-container", { duration: 1, ease: "expo.out", x: 0 });
             navOpen = true;
         } else {
-            //dropdown menu animation start
-            for (var i = 2; i <= 5; i++) {
-                $('.link' + i).css('transform', 'translateY(0)');
-            };
-            $('.nav__link').css('opacity', '0');
+            gsap.to(".nav-container", { duration: 0.75, ease: "expo.in", x: '-100%' });
+            burgerClose();
             navOpen = false;
-            
-            // nav__btn animation start   
-            $('.nav__btn-container').removeClass('open');
         }
     });
 
-    //close nav-dropdown on mobile devices
-    $('.nav__link').click(function() {
-        if(navOpen) {
-            for (var i = 2; i <= 5; i++) {
-                $('.link' + i).css('transform', 'translateY(0)');
-            };
-            $('.nav__link').css('opacity', '0');
+    //close nav when link is clicked
+    $('.nav__link-wrapper').click(() => {
+        if (navOpen) {
+            gsap.to(".nav-container", { duration: 0.75, ease: "expo.out", x: '-100%' });
+            burgerClose();
             navOpen = false;
-            
-            // nav__btn animation start   
-            $('.nav__btn-container').removeClass('open');
         }
-    });
-
-    //scrollanimation for sub-headlines (angebote)
-    var scrollLink = $('.scroll');
-
-    scrollLink.click(function (e) {
-        e.preventDefault();
-        $('body,html').animate({
-            scrollTop: $(this.hash).offset().top - 100
-            
-        }, 1000)
-        console.log($(this.hash).offset().top);
     })
 
-    //nav__link highlighting while scrolling
+    // $('.nav__link-wrapper').mouseout(function() {
+    //     turn = true;
+    // })
 
-    var navScrollLink = $('.nav__link');
+    //animation of nav__links
+    $('.nav__link-wrapper').hover(function () {
+        $(this).children(':first').toggleClass('link-up');
+        $(this).children(':last').toggleClass('bottom-down');
+    });
 
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();        
-        navScrollLink.each(function() {
-            var sectionOffset = $(this.hash).offset().top - 100;
+    //slideranimation
+    var turnCounter = 1;
+    var removeCounter = 1;
+    var addCounter = 2;
 
-            if(sectionOffset <= scrollbarLocation) {
-                $(this).addClass('active');
-                $(this).siblings().removeClass('active');
+    const runSlider = () => {
+        removeCounter = turnCounter;
+        if (turnCounter < 5) {
+            addCounter = turnCounter + 1;
+        } else {
+            addCounter = 1;
+        }
+
+        for (var i = 1; i <= 5; i++) {
+            $(".img" + i).removeClass("pos" + removeCounter);
+            $(".img" + i).addClass("pos" + addCounter);
+
+            if (removeCounter < 5) {
+                removeCounter++;
+            } else {
+                removeCounter = 1;
             }
-        })
-    })
+            if (addCounter < 5) {
+                addCounter++;
+            } else {
+                addCounter = 1;
+            }
+        }
+    }
 
-    
+    setInterval(() => {
+        runSlider();
+        if (turnCounter < 5) {
+            turnCounter++;
+        } else {
+            turnCounter = 1;
+        }
+    }, 4000);
+
+    // show selected gallery-image in modal
+    const galleryImgArr = [];
+    var imgPos;
+
+    const modal = $('.modal'),
+        modalCloseBtn = $('.modal__btn--close'),
+        imgWrapper = $('.modal__img-wrapper');
+
+    //fill galleryImgArr with src-attr of each image in gallery    
+    for (let i = 1; i <= $('.gallery__img').length; i++) {
+        galleryImgArr.push($('.gallery__img' + i).attr("src"));
+    }
+
+    const nextImg = (num) => {
+        //gsap.to(".currentImg", { duration: 0.75, ease: "expo.out", x: -100, opacity: 0 });
+        imgWrapper.empty();
+        var img = galleryImgArr[num];
+        setImg(img, getImgRatio(img));
+    }
+
+    const openModal = () => {
+        modal.removeClass('closed');
+        modal.css('opacity', '1');
+    }
+
+    const closeModal = () => {
+        modal.addClass('closed');
+    }
+
+    //every image has "acr" or "up" for across/upright set in filename
+    const getImgRatio = (attr) => {
+        var imgRatio = attr;
+        let isAcross = /acr/;
+        return isAcross.test(imgRatio);
+    }
+
+    const setImg = (src, ratio) => {
+        if (ratio) {
+            imgWrapper.append('<img src="' + src + '" class="acr currentImg">');
+        } else {
+            imgWrapper.append('<img src="' + src + '" class="up currentImg">');
+        }
+    }
+
+    $('.gallery__img').click(function () {
+        if ($(this).attr("class").length == 26) {
+            imgPos = $(this).attr("class").substr(-1);
+        } else {
+            imgPos = $(this).attr("class").substr(-2);
+        }
+
+        var img = $(this).attr("src");
+        setImg(img, getImgRatio(img));
+        openModal();
+    });
+
+    $('.modal__btn--next').click(function () {
+        if (imgPos < galleryImgArr.length) {
+            nextImg(imgPos);
+            imgPos++;
+        }
+    });
+
+    $('.modal__btn--prev').click(function () {
+        if (imgPos > 1) {
+            nextImg(imgPos - 1);
+            imgPos--;
+        }
+    });
+
+    modalCloseBtn.click(() => {
+        closeModal();
+        imgWrapper.empty();
+    });
 });
